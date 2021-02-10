22b6ac3805b7d7ba1dfdbab791beca5202ce8e42
Suzy Kaufman <skaufma5@asu.edu>
2021-02-10 04:34:33 -0700

    php addition

---
+++ form.php
@@ -0,0 +1,26 @@
+
+<?php
+	$message_sent = false;
+	if(isset ($_POST['email']) && $_POST['email'] != '') {
+
+		if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) {
+
+		$userName = $_POST['name'];
+		$userEmail = $_POST['email'];
+		$messageSubject = $_POST['subject'];
+		$message = $_POST['message'];
+
+		$to = "suzy@suzykaufman.com";
+		$body = "";
+
+		$body .= "From: ".$userName. "\r\n";
+		$body .= "Email: ".$userEmail. "\r\n";
+		$body .= "Message: ".$message. "\r\n";
+
+		mail($to,$smessageSubject, $body);
+		header("Locations: index.html");
+
+		$message_sent = true;
+		}
+	}
+?>
